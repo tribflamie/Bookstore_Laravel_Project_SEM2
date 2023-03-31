ebd1e25e54e29361141d3053916b343916b2ff0e
Sang1209 <trinhhoangphuocsang250503@gmail.com>
2023-03-31 12:47:28 +0700

    Add files via upload

---
+++ resources/views/orderDetail.blade.php
@@ -0,0 +1,57 @@
+@extends('layouts.cart-layout')
+@section('title', 'Cart - The best-selling individual books')
+@section('content')
+    <!--=== Products Start ======-->
+    <section>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="table-responsive">
+                        <table class="table table-bordered shop-cart">
+                            <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>Book</th>
+                                    <th>Name</th>
+                                    <th>Author</th>
+                                    <th>Unit Price</th>
+                                    <th>Quantity</th>
+                                    <th>Option</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php $sum=0;$quantity=0;?>
+                                <?php use App\Models\Product; ?>
+                                @if (session('orderDetails'))
+                                    <?php $orderDetails=session()->get('orderDetails');?>
+                                    @foreach ($orderDetails as $id => $order)
+                                    <?php
+                                        $product = Product::find($order->products_id);
+                                    ?>
+                                        <tr data-id="{{ $id }}" class="order">
+                                            <td>{{ $id+1 }}</td>
+                                            <td><img src="{{ asset($product->photo) }}"></td>
+                                            <td>{{$product->name}} </td>
+                                            <td>{{$product->author}}</td>
+                                            <td>${{ $product->price }}</td>
+                                            <td>{{ $order->unit_quantity }}</td>
+                                            <td><a href="#">Review</a></td>
+                                        </tr>
+                                    @endforeach
+                                @endif
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </section>
+    
+    <!--=== Products End ======-->
+@endsection
+
+@section('scripts')
+    <script type="text/javascript">
+    </script>
+@endsection
