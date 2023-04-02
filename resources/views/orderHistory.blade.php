55f750750a54cb3199fb1bb5c40e617e18ea505d
Sang1209 <trinhhoangphuocsang250503@gmail.com>
2023-04-02 16:08:54 +0700

    Update orderHistory.blade.php

--- resources/views/orderHistory.blade.php
+++ resources/views/orderHistory.blade.php
@@ -6,6 +6,19 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
+                    <select name="filter" id="filter">
+                        <?php $sortValue=array("created_at+asc","created_at+desc") ;
+                              $sortName=array("Date ascending","Date descending");
+                              $count=0;
+                        ?>
+                        @foreach ($sortValue as $val)
+                        @if($val==$filter)
+                        <option value="{{$val}}" selected>{{$sortName[$count++]}}</option>
+                        @else
+                        <option value="{{$val}}">{{$sortName[$count++]}}</option>
+                        @endif
+                        @endforeach
+                    </select>
                     <div class="table-responsive">
                         <table class="table table-bordered shop-cart">
                             <thead>
@@ -13,39 +26,41 @@
                                     <th>ID</th>
                                     <th>Preview</th>
                                     <th>Number of Products</th>
-                                    <th>Discount</th>
                                     <th>Total Price</th>
                                     <th>Status</th>
+                                    <th>Order Time</th>
                                     <th colspan="2">Option</th>
                                 </tr>
                             </thead>
                             <tbody>
                                 <?php $sum=0;$quantity=0;$couponVal=0;?>
                                 <?php use Illuminate\Support\Facades\DB; ?>
-                                @if (session('orders'))
-                                    <?php $orders=session()->get('orders');?>
+                                @if ($orders)
                                     @foreach ($orders as $id => $order)
                                     <?php
                                         $sum=0;
                                         $orderDetail=DB::table('order_details')->where('orders_id',$order->id)->get();
                                         $firstProduct=DB::table('products')->where('id',$orderDetail[0]->products_id)->first();
                                         $quantity=DB::table('order_details')->where('orders_id',$order->id)->count();
-                                        $couponVal=DB::table('coupons')->where('orders_id',$order->id)->first();
                                         foreach($orderDetail as $product):
                                             $sum+=$product->unit_quantity*$product->unit_sold_price;
                                         endforeach;
-                                        if($couponVal>0) $sum*=(1-$couponVal);
                                     ?>
                                         <tr data-id="{{ $id }}" class="order">
                                             <td>{{ $id+1 }}</td>
                                             <td><img src="{{ asset($firstProduct->photo) }}"></td>
                                             <td>{{$quantity}} </td>
-                                            <td>-${{$sum*$couponVal}}</td>
                                             <td><span>${{ $sum }}</span> </td>
-                                            <td>{{$order->status}}</td>
+                                            <td>{{$order->status}}
+                                            <?php if($order->status=='Cancelled'||$order->status=='Accepted')
+                                                echo '<br>At: '.$order->updated_at;
+                                            ?>
+                                            
+                                            </td>
+                                            <td>{{$order->created_at}}</td>
                                             <td><a href="{{ route('orderDetail',$order->id) }}">Details</a></td>
-                                            @if($order->status!='Cancelled')
-                                            <td><a href="{{ route('orderCancel',$order->id) }}">Cancel</a></td>
+                                            @if($order->status=='Processing')
+                                            <td><a href="{{ route('orderCancel',[$order->id,$filter]) }}">Cancel</a></td>
                                             @else
                                             <td>Cancel</td>
                                             @endif
@@ -66,5 +81,9 @@
 
 @section('scripts')
     <script type="text/javascript">
+        $('#filter').change(function() {
+        window.location.replace("http://127.0.0.1:8000/orderHistory/" + $(this).val());
+});
     </script>
 @endsection
+
