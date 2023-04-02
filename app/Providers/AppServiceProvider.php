52a431b22d626b0a7569233cfcc7c82e13feeab2
Sang1209 <trinhhoangphuocsang250503@gmail.com>
2023-04-02 17:11:46 +0700

    XD

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
-
+use App\Models\Category;
+use Illuminate\Support\Facades\View;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -20,6 +21,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        $categories = Category::all();
         Schema::defaultStringLength(191);
+        View::share('categories', $categories);
     }
 }
--- resources/views/layouts/layout-no-banner.blade.php
+++ resources/views/layouts/layout-no-banner.blade.php
@@ -18,7 +18,7 @@
     <div class="wrapper">
         <!--=== Header Start ======-->
         <nav class="navbar navbar-default navbar-fixed white bootsnav on no-full">
-            <div class="container">
+        <div class="container">
                 <!--=== Start Atribute Navigation ===-->
                 <div class="attr-nav">
                     <ul>
@@ -55,8 +55,8 @@
                     <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-menu">
                         <i class="icofont icofont-navigation-menu"></i> </button>
                     <div class="logo"> <a href="{{ route('home') }}"> <img class="logo logo-display"
-                                src="{{ asset('images/logo-white.png') }}" alt=""> <img
-                                class="logo logo-scrolled" src="{{ asset('images/logo-black.png') }}" alt="">
+                                src="{{ asset('images/white-logo.png') }}" alt=""> <img
+                                class="logo logo-scrolled" src="{{ asset('images/black-logo.png') }}" alt="">
                         </a> </div>
                 </div>
                 <!--=== End Header Navigation ===-->
@@ -67,134 +67,118 @@
                         <li>
                             <a href="{{ route('home') }}" class="dropdown-toggle" data-toggle="dropdown">Home</a>
                         </li>
+                        <li>
+                            <a href="{{ route('products') }}" class="dropdown-toggle"
+                                data-toggle="dropdown">Bookstore</a>
+                        </li>
                         <li class="dropdown megamenu-fw"> <a href="index.html" class="dropdown-toggle"
-                                data-toggle="dropdown">Genre</a>
+                                data-toggle="dropdown">Categories</a>
                             <ul class="dropdown-menu megamenu-content" role="menu">
                                 <li>
                                     <div class="row">
                                         <div class="col-menu col-md-3">
                                             <div class="content">
                                                 <ul class="menu-col">
-                                                    <li><a href="#">Adventure Fiction</a></li>
-                                                    <li><a href="#">Family saga</a></li>
-                                                    <li><a href="#">Historical fiction</a></li>
-                                                    <li><a href="#">Fantasy</a></li>
-                                                    <li><a href="#">Mystery</a></li>
-                                                </ul>
-                                            </div>
+                                                    @foreach ($categories as $category)
+                                                        @if ($category->id == 6)
+                                                        @break;
+                                                    @else
+                                                        <li><a
+                                                                href="{{ route('product', $category->id) }}">{{ $category->categories }}</a>
+                                                        </li>
+                                                    @endif
+                                                @endforeach
+                                            </ul>
                                         </div>
-                                        <!--=== end col-3 ===-->
-                                        <div class="col-menu col-md-3">
-                                            <div class="content">
-                                                <ul class="menu-col">
-                                                    <li><a href="#">Novella</a></li>
-                                                    <li><a href="#">Children's fiction</a></li>
-                                                    <li><a href="#">Detective</a></li>
-                                                    <li><a href="#">Coming-of-age</a></li>
-                                                    <li><a href="#">Romance</a></li>
-                                                </ul>
-                                            </div>
-                                        </div>
-                                        <!--=== end col-3 ===-->
-                                        <div class="col-menu col-md-3">
-                                            <div class="content">
-                                                <ul class="menu-col">
-                                                    <li><a href="#">Self-help</a></li>
-                                                    <li><a href="#">Magic realism</a></li>
-                                                    <li><a href="#">Novel</a></li>
-                                                    <li><a href="#">Children's novel</a></li>
-                                                    <li><a href="#">Historical novel</a></li>
-                                                </ul>
-                                            </div>
-                                        </div>
-                                        <div class="col-menu col-md-3">
-                                            <div class="content">
-                                                <ul class="menu-col">
-                                                    <li><a href="#">Sexology</a></li>
-                                                    <li><a href="#">Young adult</a></li>
-                                                    <li><a href="#">Science fiction</a></li>
-                                                    <li><a href="#">Anthropology</a></li>
-                                                    <li><a href="#">Psychology</a></li>
-                                                </ul>
-                                            </div>
-                                        </div>
-                                        <!--=== end col-3 ===-->
                                     </div>
-                                    <!--=== end row ===-->
-                                </li>
-                            </ul>
-                        </li>
-                        <li class="dropdown"> <a href="#" class="dropdown-toggle"
-                                data-toggle="dropdown">Language</a>
-                            <ul class="dropdown-menu">
-                                <li><a href="#">English</a></li>
-                                <li><a href="#">Spanish</a></li>
-                                <li><a href="#">Chinese</a></li>
-                                <li><a href="#">French</a></li>
-                                <li><a href="#">Hindi</a></li>
-                                <li><a href="#">Portuguese</a></li>
-                                <li><a href="#">German</a></li>
-                                <li><a href="#">Italian</a></li>
-                                <li><a href="#">Norwegian</a></li>
-                                <li><a href="#">Russian</a></li>
-                                <li><a href="#">Swedish</a></li>
-                                <li><a href="#">Japanese</a></li>
-                                <li><a href="#">Czech</a></li>
-                                <li><a href="#">Yiddish</a></li>
-                                <li><a href="#">Gujarati</a></li>
-                            </ul>
-                        </li>
-                        <li class="dropdown"> <a href="#" class="dropdown-toggle"
-                                data-toggle="dropdown">Published</a>
-                            <ul class="dropdown-menu">
-                                <li><a href="#">1791</a></li>
-                                <li><a href="#">1859</a></li>
-                                <li><a href="#">1937</a></li>
-                                <li><a href="#">1939</a></li>
-                                <li><a href="#">1943</a></li>
-                                <li><a href="#">1997</a></li>
-                            </ul>
-                        </li>
-                        <!-- Authentication Links -->
-                        @guest
-                            @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
-                            @endif
-
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="dropdown">
-                                <a id="navbarDropdown" class="dropdown-toggle" href="#" role="button"
-                                    data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
-
-                                <ul class="dropdown-menu">
-                                    <li><a>Account Setting</a></li>
-                                    <li><a>Order History</a></li>
-                                    <li><a href="{{ route('feedbacks') }}">My Reviews</a></li>
-                                    <li><a href="{{ route('logout') }}"
-                                            onclick="event.preventDefault();
-                                                             document.getElementById('logout-form').submit();">
-                                            {{ __('Logout') }}
-                                        </a></li>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST"
-                                        class="d-none">
-                                        @csrf
-                                    </form>
-                                </ul>
+                                    <!--=== end col-3 ===-->
+                                    <div class="col-menu col-md-3">
+                                        <div class="content">
+                                            <ul class="menu-col">
+                                                <li><a href="#">Self-help</a></li>
+                                                <li><a href="#">Magic realism</a></li>
+                                                <li><a href="#">Novel</a></li>
+                                                <li><a href="#">Children's novel</a></li>
+                                                <li><a href="#">Historical novel</a></li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                    <div class="col-menu col-md-3">
+                                        <div class="content">
+                                            <ul class="menu-col">
+                                                <li><a href="#">Sexology</a></li>
+                                                <li><a href="#">Young adult</a></li>
+                                                <li><a href="#">Science fiction</a></li>
+                                                <li><a href="#">Anthropology</a></li>
+                                                <li><a href="#">Psychology</a></li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                    <!--=== end col-3 ===-->
+                                </div>
+                                <!--=== end row ===-->
                             </li>
-                        @endguest
-                    </ul>
-                </div>
-                <!--=== /.navbar-collapse ===-->
+                        </ul>
+                    </li>
+                    <li class="dropdown"> <a href="#" class="dropdown-toggle"
+                            data-toggle="dropdown">Country</a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">English</a></li>
+                            <li><a href="#">Spanish</a></li>
+                            <li><a href="#">Chinese</a></li>
+                            <li><a href="#">French</a></li>
+                            <li><a href="#">See More</a></li>
+                        </ul>
+                    </li>
+                    <li class="dropdown"> <a href="#" class="dropdown-toggle"
+                            data-toggle="dropdown">Published</a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">1791</a></li>
+                            <li><a href="#">1859</a></li>
+                            <li><a href="#">1937</a></li>
+                            <li><a href="#">1939</a></li>
+                            <li><a href="#">See More</a></li>
+                        </ul>
+                    </li>
+                    <!-- Authentication Links -->
+                    @guest
+                        @if (Route::has('login'))
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            </li>
+                        @endif
+
+                        @if (Route::has('register'))
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                            </li>
+                        @endif
+                    @else
+                        <li class="dropdown">
+                            <a id="navbarDropdown" class="dropdown-toggle" href="#" role="button"
+                                data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                {{ Auth::user()->name }}
+                            </a>
+
+                            <ul class="dropdown-menu">
+                                <li><a href="{{ route('edit.profile') }}">Edit Profile</a></li>
+                                <?php $filter='a';?>
+                                <li><a href="{{ route('orderHistory',$filter)}}">Order History</a></li>
+                                <li><a href="{{ route('feedbacks') }}">Feedbacks</a></li>
+                                <li><a href="{{ route('logout') }}"
+                                        onclick="event.preventDefault();
+                                                             document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a></li>
+
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                    class="d-none">
+                                    @csrf
+                                </form>
+                            </ul>
+                        </li>
+                    @endguest
+                </ul>
             </div>
         </nav>
         <!--=== Header End ======-->
